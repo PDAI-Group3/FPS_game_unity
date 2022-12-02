7bac68c78f27e2cd228806fc45979f0c95d84c37
Luukas <78479063+luukasmepham@users.noreply.github.com>
2022-12-02 10:48:09 +0200

    Gun spawning in multiplayer working now

--- Assets/Scripts/Networking/NetworkPlayer/NetworkPlayerLook.cs
+++ Assets/Scripts/Networking/NetworkPlayer/NetworkPlayerLook.cs
@@ -6,6 +6,8 @@ using Unity.Netcode;
 public class NetworkPlayerLook : NetworkBehaviour
 {
     public Camera cam;
+
+    public Transform weapon;
     private float xRotation = 0f;
 
     public float xSensitivity = 30f;
@@ -24,14 +26,14 @@ public class NetworkPlayerLook : NetworkBehaviour
     }
     public void ProcessLook(Vector2 input)
     {
-        float mouseX =input.x;
-        float mouseY =input.y;
+        float mouseX = input.x;
+        float mouseY = input.y;
 
         xRotation -= (mouseY * Time.deltaTime) * ySensitivity;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         cam.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-
+        
         transform.Rotate(Vector3.up * (mouseX * Time.deltaTime) * xSensitivity);
     }
 }
